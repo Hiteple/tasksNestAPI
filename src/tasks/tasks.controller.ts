367178ed596452637b4016d5cc401f8adcd81274
hiteple <hiteplegold@gmail.com>
2021-01-18 23:38:56 -0300

    Adding task creation functionality

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ITask } from './tasks.interfaces';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,10 @@ export class TasksController {
    @Get()
    public getAllTasks(): ITask[] {
       return this.tasksService.getAllTasks();
+   }
+
+   @Post()
+   public createTask(@Body('title') title: string, @Body('description') description: string): ITask {
+      return this.tasksService.createTask(title, description);
    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { ITask } from './tasks.interfaces';
+import { ITask, TaskStatus } from './tasks.interfaces';
+import { v4 as uuidv4} from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +8,17 @@ export class TasksService {
 
    public getAllTasks(): ITask[] {
       return this.tasks;
+   }
+
+   public createTask(title: string, description: string): ITask {
+      const task: ITask = {
+         id: uuidv4(),
+         title,
+         description,
+         status: TaskStatus.OPEN
+      };
+
+      this.tasks.push(task);
+      return task;
    }
 }
